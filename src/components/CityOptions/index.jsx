7aca9f5bc2d8d8d7e7ff7bbea524a4e86a65454b
Lucie Dušková <lucie.duskova87@gmail.com>
2023-11-11 12:13:23 +0100

    Seznam měst z API

---
+++ src/components/CityOptions/index.jsx
@@ -0,0 +1,14 @@
+
+
+
+
+export const CityOptions = () => {
+
+
+
+  return(
+<option>
+  </option>
+  )
+
+}
--- src/components/JourneyPicker/index.jsx
+++ src/components/JourneyPicker/index.jsx
@@ -1,20 +1,45 @@
 import React, { useEffect, useState } from 'react';
 import './style.css';
 
-
+export const CityOptions = ({ cities }) => {
+  return (
+    <>
+      <option value="">Vyberte</option>
+      {cities.map((city) => (
+        <option key={city.code} value={city.code}>
+          {city.name}
+        </option>
+      ))}
+    </>
+  );
+};
 
 export const JourneyPicker = ({ onJourneyChange }) => {
   const [fromCity, setFromCity] = useState('');
   const [toCity, setToCity] = useState('');
   const [date, setDate] = useState('');
+  const [cities, setCities] = useState([]);
+
+  useEffect(() => {
+    const fetchName = async () => {
+      const response = await fetch(
+        'https://apps.kodim.cz/daweb/leviexpress/api/cities',
+      );
+      const data = await response.json();
+      console.log(data);
+
+      setCities(data.results);
+    };
+
+    fetchName();
+  }, []);
 
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log(date);
     console.log(toCity);
-    console.log(fromCity);
+    console.log(cities);
   };
-
 
   return (
     <div className="journey-picker container">
@@ -29,12 +54,7 @@ export const JourneyPicker = ({ onJourneyChange }) => {
                 setFromCity(e.target.value);
               }}
             >
-              <option value="">Vyberte</option>
-              <option value="mesto01">Město 01</option>
-              <option value="mesto02">Město 02</option>
-              <option value="mesto03">Město 03</option>
-              <option value="mesto04">Město 04</option>
-              <option value="mesto05">Město 05</option>
+              <CityOptions cities={cities} />
             </select>
           </label>
           <label>
@@ -45,12 +65,7 @@ export const JourneyPicker = ({ onJourneyChange }) => {
                 setToCity(e.target.value);
               }}
             >
-              <option value="">Vyberte</option>
-              <option value="mesto01">Město 01</option>
-              <option value="mesto02">Město 02</option>
-              <option value="mesto03">Město 03</option>
-              <option value="mesto04">Město 04</option>
-              <option value="mesto05">Město 05</option>
+              <CityOptions cities={cities} />
             </select>
           </label>
           <label>
