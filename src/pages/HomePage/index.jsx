a628f5dee0a333f0b2be4630b60c432f63ed1325
Lucie Dušková <lucie.duskova87@gmail.com>
2023-11-11 16:59:49 +0100

    Potvrzení rezervace-1OK

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -3,24 +3,44 @@ import { JourneyPicker } from '../../components/JourneyPicker';
 import { JourneyDetail } from '../../components/JourneyDetail';
 import { SelectedSeat } from '../../components/SelectedSeat';
 
-
-
 export const HomePage = () => {
-  const [journey, setJourney] = useState(null)
+  const [journey, setJourney] = useState(null);
 
-  const handleJourneyChange = (journey) =>{
-    setJourney(journey)
-    }
-    
-    if(journey=='')
-    console.log(journey)
+  const handleJourneyChange = (journey) => {
+    setJourney(journey);
+  };
+
+  if (journey == '') console.log(journey);
+
+  const handleBuy = (e) => {
+    e.preventDefault()
+    console.log('bla bla')
+  }
 
   return (
     <main>
+      <div className="controls container">
+        <button className="btn btn--big" type="button">
+          Rezervovat
+        </button>
+      </div>
+
       <JourneyPicker value={journey} onJourneyChange={handleJourneyChange} />
 
+      {journey !== null ? (
+        <>
+          <JourneyDetail journey={journey} />{' '}
+          <SelectedSeat number={journey.autoSeat} />
+        </>
+      ) : (
+        ''
+      )}
 
-      {journey !== null ? <><JourneyDetail journey={journey} /> <SelectedSeat number={journey.autoSeat}/></> : ''}
+      <div class="controls container">
+        <button onClick={handleBuy} class="btn btn--big" type="button">
+          Rezervovat
+        </button>
+      </div>
     </main>
   );
 };
