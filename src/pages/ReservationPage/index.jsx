32958750730327369c08804188d0272e22188e5d
Lucie Dušková <lucie.duskova87@gmail.com>
2023-11-11 10:19:36 +0100

    Vytvoření Reservation + menu

---
+++ src/pages/ReservationPage/index.jsx
@@ -0,0 +1,11 @@
+import { JourneyPicker } from '../../components/JourneyPicker';
+
+export const ReservationPage = () => {
+  return (
+    <p>Tady bude rezervace</p>
+
+    // <main>
+    //   <JourneyPicker />
+    // </main>
+  );
+};
